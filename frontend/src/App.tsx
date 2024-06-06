570795e3cbf59b3b046b997f14d47306e2d81570
mdemo <mdemong1@gmail.com>
2024-06-06 14:08:35 -0700

    Added desktop background

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
 
-import original from 'react95/dist/themes/original';
+import original from "react95/dist/themes/peggysPastels";
 
-import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
-import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
-import { Window, WindowContent, WindowHeader, styleReset } from 'react95';
+import ms_sans_serif from "react95/dist/fonts/ms_sans_serif.woff2";
+import ms_sans_serif_bold from "react95/dist/fonts/ms_sans_serif_bold.woff2";
+import { Window, WindowContent, WindowHeader, styleReset } from "react95";
 
 const GlobalStyles = createGlobalStyle`
   ${styleReset}
@@ -25,22 +25,31 @@ const GlobalStyles = createGlobalStyle`
   }
 `;
 
+const Wrapper = styled.div`
+  height: 100vh;
+  padding: 5rem;
+  box-sizing: border-box;
+  background: ${({ theme }) => theme.desktopBackground};
+`;
+
 function App() {
   return (
-    <> 
-      <GlobalStyles/>
+    <>
+      <GlobalStyles />
       <ThemeProvider theme={original}>
-        <Window>
-          <WindowHeader>
-            <span>DogChat</span>
-          </WindowHeader>
-          <WindowContent>
-            <p>what a cool window</p>
-          </WindowContent>
-        </Window>
+        <Wrapper>
+          <Window>
+            <WindowHeader>
+              <span>DogChat</span>
+            </WindowHeader>
+            <WindowContent>
+              <p>what a cool window</p>
+            </WindowContent>
+          </Window>
+        </Wrapper>
       </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
